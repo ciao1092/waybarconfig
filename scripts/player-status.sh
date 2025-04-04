32a36efb837065585ccc0d4bcf21970d62146a46
Carlo <100089228+ciao1092@users.noreply.github.com>
2025-04-04 15:56:51 +0200

    Add custom media plugin

---
+++ scripts/player-status.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+status=$(playerctl status 2>/dev/null)
+
+trunc() {
+    if [ "${#1}" -le 13 ]; then
+        echo "$1"
+    else
+        printf '%s…%s\n' "${1%%"${1#??????}"}" "${1##"${1%??????}"}"
+    fi
+}
+
+title="$(playerctl metadata title 2>/dev/null)"
+
+title_no_ytm="${title//\ \-\ YouTube Music/}"
+
+if [[ "x${title_no_ytm}" != "x" ]]; then
+	title="${title_no_ytm}"
+fi
+
+title=$(trunc "${title}")
+
+if [[ "x${title}" != "x" ]]; then
+	title=" $title "
+fi
+
+if [[ "$status" == "Playing" ]]; then
+    echo '{"text": "  '"$title"'", "class": "playing"}'
+else
+    echo '{"text": "  '"$title"'", "class": "paused"}'
+fi
